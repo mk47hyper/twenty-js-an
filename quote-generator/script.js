a334f42b96aac1c03ec6c2133e9d9c0e33902328
mk47hyper <mkhyper17@gmail.com>
2021-05-10 14:40:42 +0100

    Removed comments .

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -7,66 +7,60 @@ const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-//Show Loading
-function loading() {
+function showLoadingSpinner() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-//Hide Loading
-function complete() {
+function removeLoadingSpineer() {
   quoteContainer.hidden = false;
   loader.hidden = true;
 }
 
 // Show New Quote
 function newQuote() {
-  loading();
-  //Pick a random quote from apiQuotes array
+  showLoadingSpinner();
+  // Pick a random quote from array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  // const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-
-  //Check if author field is blank and replace it with "Unknown"
+  // Check if Author field is blank and replace it with 'Unknown'
   if (!quote.author) {
     authorText.textContent = 'Unknown';
   } else {
     authorText.textContent = quote.author;
   }
-  //Check quote length to determine styling
+  // Check Quote length to determine styling
   if (quote.text.length > 120) {
     quoteText.classList.add('long-quote');
   } else {
     quoteText.classList.remove('long-quote');
   }
-  //Set Quote, Hide Loader
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
-  complete();
+  removeLoadingSpineer();
 }
 
 // Get Quotes From API
 async function getQuotes() {
-  loading();
+  showLoadingSpinner();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote();
   } catch (error) {
-    //Catch Error Here
-    alert(error);
+    // Catch Error Here
   }
 }
 
-//Tweet Quote
+// Tweet Quote
 function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
   window.open(twitterUrl, '_blank');
 }
 
-//Event Listeners
+// Event Listeners
 newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
-// newQuote();
